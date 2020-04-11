41d2badbb197f1d3d2c90fad90318e7f305017f9
Alex <alexbn@uci.edu>
2020-04-10 20:09:24 -0700

    First commit

---
+++ src/SingleMovieServlet.java
@@ -0,0 +1,79 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet(name = "SingleMovieServlet", urlPatterns = "/api/single-movie")
+public class SingleMovieServlet extends HttpServlet {
+	@Resource(name = "jdbc/moviedbexample")
+	private DataSource dataSource;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("application/json");
+		String id = request.getParameter("id");
+		PrintWriter out = response.getWriter();
+
+		try {
+			Connection dbcon = dataSource.getConnection();
+			String query = "SELECT DISTINCT id as 'id', title as 'title', year, director as 'director', name as 'name', " +
+					"starId as 'starId', rating as 'rating', genres as 'genres' " +
+					"FROM singlemovie WHERE id = ?;";
+
+			PreparedStatement statement = dbcon.prepareStatement(query);
+			statement.setString(1, id);
+			ResultSet rs = statement.executeQuery();
+			JsonArray jsonArray = new JsonArray();
+
+			while (rs.next()) {
+				String movieId = rs.getString("id");
+				String movieTitle = rs.getString("title");
+				String movieDir = rs.getString("director");
+				Integer movieYear = rs.getInt("year");
+				String starName = rs.getString("name");
+				Double movieRating = rs.getDouble("rating");
+				String movieGenre = rs.getString("genres");
+				String starId = rs.getString("starId");
+
+				JsonObject jsonObject = new JsonObject();
+				jsonObject.addProperty("movie_id", movieId);
+				jsonObject.addProperty("movie_title", movieTitle);
+				jsonObject.addProperty("movie_dir", movieDir);
+				jsonObject.addProperty("movie_year", movieYear);
+				jsonObject.addProperty("movie_rating", movieRating);
+				jsonObject.addProperty("movie_genre", movieGenre);
+				jsonObject.addProperty("star_id", starId);
+				jsonObject.addProperty("star_name", starName);
+
+				jsonArray.add(jsonObject);
+			}
+
+            out.write(jsonArray.toString());
+            response.setStatus(200);
+
+			rs.close();
+			statement.close();
+			dbcon.close();
+		} catch (Exception e) {
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("errorMessage", e.getMessage());
+
+			out.write(jsonObject.toString());
+			response.setStatus(500);
+		}
+		out.close();
+
+	}
+
+}
